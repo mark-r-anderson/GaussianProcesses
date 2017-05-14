8073cb510a4c3fcde7cbfaa3bd1ee4677e9d3863
Mark Anderson <mark.russ.anderson@gmail.com>
2017-05-13 20:15:18 -0400

    cleaned up comments

--- GPR.py
+++ GPR.py
@@ -1,6 +1,5 @@
 import numpy as np
-
-from scipy.optimize import minimize, rosen, rosen_der
+from scipy.optimize import minimize
 
 ###################
 #Gaussian processes
@@ -29,35 +28,37 @@ class GPR:
         self.set_K()
         
     def compute_K(self,x1,x2):
+        '''
+        Recompute K, K_inv and return the result.
+
+        TODO:
+        -add positive definite check
+        '''
+        #Compute the covariance matrix.
         K = self.kernel.get_cov_mat(x1,x2)
+
+        #Compute the inverse of the covariance matrix.
         K_inv = np.linalg.inv(K)
 
         return K, K_inv
 
     def set_K(self):
+        '''
+        Recompute K, K_inv and set the attributes to the result.
+        '''
         self.K, self.K_inv = self.compute_K(self.x,self.x)
         
-    def predict(self,x_star,returnCov = True):
+    def predict(self,x_star,returnCov=True):
         '''
         Given a model and a set of independent observations, predict the corresponding dependent variable values.
         '''
-        #Compute the covariance matrix between the test data and the training data
-        #Each row corresponds to one test point
+        #Compute the covariance matrix between the test data and the training data.
         K_star = self.kernel.get_cov_mat(self.x,x_star)
 
-        #Compute the covariance matrix between the training data and itself
-        #(each diagonal element is the covriance matrix for a single test point)
+        #Compute the covariance matrix between the training data and itself.
         K_star_star = self.kernel.get_cov_mat(x_star,x_star)
-
-        #print(K_star * self.K_inv)
-        #print(self.y)
-        #print(K_star * self.K_inv * self.y[:,np.newaxis])
-        #print('')
-        #print(np.matrix(self.y))
-        #print(K_star * self.K_inv * np.transpose(np.matrix(self.y)))
         
         #Compute the mean (each row of K_star adds another element to the array)
-        #y_star_mean = np.array( K_star * self.K_inv * self.y[:,np.newaxis] )
         y_star_mean = np.array( K_star * self.K_inv * np.transpose(np.matrix(self.y)) )
         
         #Remove the extra dimension for purposes of plotting later on (not really required, but convenient)
@@ -79,11 +80,13 @@ class GPR:
         Draw a desired number of samples from a Gaussian process prior.
         Based on the given kernel function. No training data.
         '''
-        #generate the mean for prior distribution(assumed to be zero)
+        #Generate the mean for the prior distribution (assumed to be zero).
         m = np.transpose( np.matrix( np.zeros(x_star.size) ) )
-        #generate the covariance matrix for prior distribution
-        K = self.kernel.get_cov_mat(x_star)
         
+        #Generate the covariance matrix for prior distribution.
+        K = self.kernel.get_cov_mat(x_star)
+
+        #Return n samples for distribution of N~(m,K)
         return self.get_samples(m,K,n)
     
     def sample_from_posterior(self,x_star,n=1):
@@ -91,11 +94,13 @@ class GPR:
         Draw a desired number of samples from a Gaussian process posterior.
         Based on the given kernel function and the traning data.
         '''
-        #get the mean and the covariance matrix of the posterior distribution
+        #Generate the mean and the covariance matrix for the posterior distribution.
         m,K = self.predict(x_star)
-        #convert to matrix format
-        m = np.transpose( np.matrix(m) )
         
+        #Convert the mean to matrix format.
+        m = np.transpose( np.matrix(m) )
+
+        #Return n samples for distribution of N~(m,K)
         return self.get_samples(m,K,n)
 
     def optimize(self):
@@ -107,59 +112,50 @@ class GPR:
         -Test this method way more to ensure it performs as expected
         -Ensure that set_hyperparameters updates the kernel hyperparameters as well
         '''
-        #Set the initial hyperparameters to the ones entered by the user
+        #Set the initial hyperparameters to the ones entered by the user.
         hparams0 = self.kernel.get_hyperparameters()
-        #hparams0 = [self.kernel.hparams,self.kernel.hparams,self.kernel.hparams]
-
-        #print(hparams0)
-        #print(type(hparams0))
         
-        #Minimize the negative log marginal likelihood based on these initial parameters
+        #Minimize the negative log marginal likelihood based on these initial parameters.
         res = minimize(self.lml,hparams0,method='Nelder-Mead', tol=1e-6)
 
-        #print(res.x)
-        #Update the hyperparameters with the optimal version
-        #self.kernel.set_hyperparameters(res.x)
-
-        #Update the covariance matrices for the training data (test matrices updated in predict method)
+        #Update the covariance matrices for the training data (test matrices updated in predict method).
         self.set_K()
-        
-        #OPTIONS
-        #create new LML class, instantiate a class and pass its bound method to minimize
-        #define the LML function inside optimize()
-        #result = minimize(self.lml,x0,method='Nelder-Mead', tol=1e-6,args=(self))
-        
+
+        #If desired, can return the resulting object from SciPy optimize.
         return res
 
     def lml(self,hparams):
         '''
         Negative log marginal likelihood.
-        '''
 
-        #print("GFSDGSD")
-        
+        TODO:
+        -make hparams argument optional and use user-entered parameters instead
+        '''
         #Reassign hyperparameters
         self.kernel.set_hyperparameters(hparams)
 
         #Covariance matrix must be recomputed and inverted every time!
-        K = self.kernel.get_cov_mat(self.x)
-        K_inv = np.linalg.inv(K)
-        
+        #K = self.kernel.get_cov_mat(self.x)
+        #K_inv = np.linalg.inv(K)
+        K,K_inv = self.compute_K(self.x,self.x)
+
+        #Return the negative log marginal likelihood (scalar).
         return np.asscalar( np.matrix(self.y)*K_inv*np.transpose(np.matrix(self.y)) + np.linalg.det(K) )
     
     def get_samples(self,m,K,n=1):
         '''
         General method to sample from a distribution with mean m and covariance matrix K.
         Also accepts the number of samples desired.
+        Returns n samples drawn from distribution of N~(m,K)
 
         TODO:
         -Fix square root
         -Add positive definite check and loop
         '''
-        #identity matrix
+        #Generate the identity matrix.
         I = np.matrix( np.identity( int(np.sqrt(K.size)) ) )
         
-        #add a bit of "noise" because the Cholesky decomposition may not work
+        #Add a bit of "noise" because the Cholesky decomposition may not work.
         K += 0.0000001*I
         
         #add positive definite check
@@ -167,20 +163,15 @@ class GPR:
         # K+= i*0.000000000001*I or something like that
         #must have some sort of limit so it isn't adding huge numbers
 
-        #compute the Cholesky decomposition
+        #Compute the Cholesky decomposition.
         L = np.matrix( np.linalg.cholesky(K) )
         
-        #generate random samples with mean m and covariance of the identity matrix (i.e., independent)
+        #Generate random samples with mean **0** and covariance of the identity matrix (i.e., independent).
         u = np.transpose( np.matrix( np.diag( np.random.normal(0,I) ) ) )
-        
-        #print(u)
-        #print(m)
-        #print(u+m)
-             
+
+        #Generate the desired number of samples.
         for i in range(1,n,1):
             u = np.append( u,np.transpose(np.matrix(np.diag(np.random.normal(0,I)))),axis=1 )
-        
-        #print(m)
-        #print(L*u)
-        
+
+        #Return the sample(s) with distribution N~(m,K).
         return m + L*u
--- Kernel.py
+++ Kernel.py
@@ -11,36 +11,45 @@ class Kernel(ABC):
     '''
     def __init__(self):
         pass
-        
-    #def set_kernel_hyperparameters(self,hparams):
-        #self.hparams = hparams
-        #pass
-
+    
     @abstractmethod
     def get_size(self):
+        '''
+        Return the number of hyperparameters in the kernel.
+        '''
         pass
         
     @abstractmethod
     def set_hyperparameters(self):
+        '''
+        Set the kernel hyperparameters.
+        '''
         pass
 
     @abstractmethod
     def get_hyperparameters(self):
+        '''
+        Return the kernel hyperparameters.
+        '''
         pass
     
     @abstractmethod
     def compute(self,x1,x2):
+        '''
+        Compute the covariance matrix between two points.
+        Must be implemented seperately for each kernel as every kernel has a different equation.
+        '''
         pass
 
     def __add__(self,kernel):
         '''
-        Summation of self and kernel
+        Summation of self and kernel.
         '''
         return CombinedKernel(self,kernel,"add")
 
     def __mul__(self,kernel):
         '''
-        Multiplication of self and kernel
+        Multiplication of self and kernel.
         '''
         return CombinedKernel(self,kernel,"mul")
     
@@ -52,7 +61,7 @@ class Kernel(ABC):
         Dimension of 2nd argument is the number of rows
         
         TODO:
-        -
+        -optimize
         '''        
         if x2 == None:
             x_mn,x_nm = np.meshgrid(x1,x1)
@@ -61,9 +70,13 @@ class Kernel(ABC):
     
         return np.matrix( self.compute(x_mn,x_nm) )
 
+##################################################################################################
+#Subkernel classes BasicKernel and CombinedKernel which represent the two general types of kernels
+##################################################################################################
+    
 class BasicKernel(Kernel):
     '''
-    Class to hold basic kernels.
+    Class to hold basic (i.e., not combined) kernels.
     '''
     def __init__(self,*args):
         Kernel.__init__(self)
@@ -83,7 +96,7 @@ class BasicKernel(Kernel):
             
 class CombinedKernel(Kernel):
     '''
-    Class to hold the combination of two kernels (e.g., sum, multiplication, etc.)
+    Class to hold the combination of two kernels (e.g., sum, multiplication, etc.).
     '''
     def __init__(self,kernel1,kernel2,operation):
         Kernel.__init__(self)
@@ -121,7 +134,6 @@ class CombinedKernel(Kernel):
         Each list and sublist respresents a kernel.
         The innermost list will contain the arrays for the two basic kernels.
         '''
-
         #return [self.kernel1.get_hyperparameters(),self.kernel2.get_hyperparameters()]
         #return np.array([self.kernel1.get_hyperparameters(),self.kernel2.get_hyperparameters()],dtype=object)
 
@@ -155,7 +167,6 @@ class SqExp(BasicKernel):
         
     def compute(self,x1,x2):
         '''
-        Takes as inputs two numpy arrays (only accepts 1 feature currently).
         Return squared exponential result as a numpy array.
         '''
         
@@ -180,8 +191,7 @@ class RQ(BasicKernel):
         
     def compute(self,x1,x2):
         '''
-        Takes as inputs two numpy arrays (only accepts 1 feature currently).
-        Return squared exponential result as a numpy array.
+        Return rational quadratic result as a numpy array.
         '''
         
         return self.variance**2 * ( 1 + np.exp( -(x1-x2)**2 / (2*self.alpha*self.lengthscale**2) ) )
@@ -204,8 +214,7 @@ class ExpSine(BasicKernel):
         
     def compute(self,x1,x2):
         '''
-        Takes as inputs two numpy arrays (only accepts 1 feature currently).
-        Return squared exponential result as a numpy array.
+        Return exponential sine (periodic) result as a numpy array.
         '''
         
         return self.variance**2 * np.exp( - 2*np.sin(np.pi*abs(x1-x2)/self.period) / (self.lengthscale**2) )
